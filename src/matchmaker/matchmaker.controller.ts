ef706230e4f90a713acd88138ff8e794f6c64552
Yuri Stepin <enchantinggg4@gmail.com>
2025-01-20 15:26:20 +0300

    query

--- src/matchmaker/matchmaker.controller.ts
+++ src/matchmaker/matchmaker.controller.ts
@@ -1,17 +1,27 @@
 import { Controller } from "@nestjs/common";
-import { EventPattern } from "@nestjs/microservices";
+import { EventPattern, MessagePattern } from "@nestjs/microservices";
 import { PlayerEnterQueueRequestedEvent } from "@/gateway/events/mm/player-enter-queue-requested.event";
 import { construct } from "@/gateway/util/construct";
-import { EventBus } from "@nestjs/cqrs";
+import { EventBus, QueryBus } from "@nestjs/cqrs";
+import { GetPartyQueryResult } from "@/gateway/queries/GetParty/get-party-query.result";
+import { GetPartyQuery } from "@/gateway/queries/GetParty/get-party.query";
 
 @Controller()
 export class MatchmakerController {
-  constructor(private readonly ebus: EventBus) {}
+  constructor(
+    private readonly ebus: EventBus,
+    private readonly qbus: QueryBus,
+  ) {}
 
   @EventPattern(PlayerEnterQueueRequestedEvent.name)
   public async PlayerEnterQueueRequestedEvent(
     cmd: PlayerEnterQueueRequestedEvent,
   ) {
     await this.ebus.publish(construct(PlayerEnterQueueRequestedEvent, cmd));
+  }
+
+  @MessagePattern(GetPartyQuery.name)
+  async GetPartyQuery(query: GetPartyQuery): Promise<GetPartyQueryResult> {
+    return this.qbus.execute(construct(GetPartyQuery, query));
   }
 }
