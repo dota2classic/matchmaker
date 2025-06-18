28cc431ac9f9c521d5f7ffb6cd1fbc91a7fe9908
Yuri Stepin <enchantinggg4@gmail.com>
2025-06-18 19:00:05 +0300

    fix

--- src/generated-api/mapped-models/index.ts
+++ src/generated-api/mapped-models/index.ts
@@ -1,1 +1,11 @@
-export * from "./MatchAccessLevel"
+export * from "./MatchmakingMode";
+export * from "./MMMatchmakingMode";
+export * from "./ThreadType";
+export * from "./Dota_GameMode";
+export * from "./Dota_Map";
+export * from "./GameserverAchievementKey";
+export * from "./BanReason";
+export * from "./Role";
+export * from "./PlayerAspect";
+export * from "./MatchAccessLevel";
+
